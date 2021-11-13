1acea7dd7de6c8bc48f592e4b98f9131f07bf4f9
Marcin <mkozera024@gmail.com>
2021-11-13 14:11:03 +0100

    Adding new pages

---
+++ screens/HelpScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+const HelpScreen = () => {
+    return(
+        <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+            <Text>Pomoc (na razie testowo)
+            </Text>
+        </View>
+    )
+}
+
+export default HelpScreen
---
+++ screens/ProductScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+const ProductScreen = () => {
+    return(
+        <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+            <Text>Strona z produktami</Text>
+        </View>
+    )
+}
+
+export default ProductScreen
