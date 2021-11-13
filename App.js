26dd9176e7ead81eadbcd69298e0da8f1ff0c449
Marcin <mkozera024@gmail.com>
2021-11-13 14:11:33 +0100

    Adding drawer navigation

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function App() {
+import HelpScreen from './screens/HelpScreen.js'
+import ProductScreen from './screens/ProductScreen.js'
+
+function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Witaj świecie! Dziś zaczynamy pracę nad projektem! :D</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Strona domowa</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Drawer = createDrawerNavigator();
+
+const Navigation = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Help" component={HelpScreen} />
+      <Drawer.Screen name="Products" component={ProductScreen} />
+    </Drawer.Navigator>
+  );
+}
+
+export default Navigation
